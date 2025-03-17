97b2d48467ffaffc772798eaab3254843a60ceaa
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-03-17 11:52:16 +0200

    jump solution

--- jump_game2.js
+++ jump_game2.js
@@ -1,30 +1,22 @@
 var jump = function (nums) {
-  let farthest = 0;
-  let j = 0;
+  let n = nums.length;
+  if (n === 1) return 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (i > farthest) return false;
-    else if (farthest < i + nums[i]) {
-      if (farthest >= nums.length - 1) return j;
-      else {
-        j++;
-        farthest = i + nums[i];
-      }
-      if (farthest >= nums.length - 1) return j;
-    } else if (farthest > i + nums[i]) j--;
+  let jumps = 0;
+  let currentEnd = 0;
+  let farthest = 0;
+
+  for (let i = 0; i < n - 1; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+
+    if (i === currentEnd) {
+      // Time to jump
+      jumps++;
+      currentEnd = farthest;
+
+      if (currentEnd >= n - 1) break;
+    }
   }
+
+  return jumps;
 };
-const nums6 = [3, 2, 1, 0, 4];
-const nums = [2, 3, 1, 1, 4];
-const nums3 = [2, 5, 0, 0];
-const nums2 = [
-  2, 0, 6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2, 1, 2, 6, 5,
-  3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0, 3, 8, 2, 4, 0, 1, 2, 0, 1, 4, 6, 5, 8, 0,
-  7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0, 4, 9, 0, 9, 8, 4, 3, 0, 7, 7, 1, 9,
-  1, 9, 4, 9, 0, 1, 9, 5, 7, 7, 1, 5, 8, 2, 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6,
-];
-const nums4 = [2, 3, 1, 1, 4];
-const nums5 = [2, 5, 0, 0];
-const nums1 = [0];
-console.log(jump(nums));
-const arr1 = [[0, 0]];
--- jump_second_try.js
+++ jump_second_try.js
@@ -1,27 +1,20 @@
 var canJump = function (nums) {
   let farthest = 0;
   let j = 0;
-  maxfarthest = -1;
-  if (nums.length == 1) {
-    return j;
-  }
-  if (nums[0] >= nums.length) {
-    return j + 1;
-  }
   for (let i = 0; i < nums.length; i++) {
-    if (farthest < i + nums[i]) {
-      farthest = i + nums[i];
-      if (
-        farthest >= nums.length - 1 ||
-        maxfarthest < nums[farthest] + farthest
-      ) {
-        j++;
-        if (farthest > nums.length - 1) return j;
-        maxfarthest = nums[farthest] + farthest;
-        if (maxfarthest >= nums.length - 1) return j + 1;
-      }
+    if (i > farthest) {
+      return false;
+    }
+
+    farthest = Math.max(farthest, i + nums[i]);
+    j++;
+    console.log(j);
+    if (farthest >= nums.length - 1) {
+      return true;
     }
   }
+
+  return false;
 };
-const nums = [2, 0, 8, 0, 3, 4, 7, 5, 6, 1, 0, 0, 5, 9, 7, 5, 3, 6];
+const nums = [3, 2, 1, 2, 5, 13, 8, 85, 4];
 console.log(canJump(nums));
