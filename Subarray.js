4f1a45cb4596b959fb712d7c572233ffbfcb247b
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-03-18 14:13:59 +0200

    mini subaaray

--- Subarray.js
+++ Subarray.js
@@ -4,40 +4,24 @@
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
-  let sum = 0;
-  let j = 0;
-  let subarray = [];
-  let small = Infinity;
-  let i = 0;
-  while (j < nums.length) {
-    if (sum < target && i < nums.length) sum += nums[i];
+  let left = 0,
+    sum = 0;
+  let minLength = Infinity;
 
-    if (sum > target) {
-      sum -= nums[j];
-      j++;
-    }
-    if (sum >= target && i - j < small) {
-      small = i - j;
-      subarray = [];
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right]; // Expand the window
 
-      for (let x = j; x < i + 1; x++) {
-        subarray.push(nums[x]);
-      }
-      sum -= nums[j];
-      j++;
-      console.log(subarray);
-    }
-    if (i < nums.length) {
-      i++;
-    } else {
-      j++;
+    // Shrink the window from the left while sum is still valid
+    while (sum >= target) {
+      minLength = Math.min(minLength, right - left + 1);
+      sum -= nums[left]; // Remove leftmost element
+      left++; // Shrink the window
     }
   }
-  if (subarray.length == 0) {
-    return 0;
-  }
-  return subarray.length;
+
+  return minLength === Infinity ? 0 : minLength;
 };
+
 let target = 11;
 nums = [1, 1, 1, 1, 1, 1, 1, 1];
 console.log(minSubArrayLen(target, nums));
