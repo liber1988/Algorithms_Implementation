3b8ce7ca0fd440be888d824d2568c291ce11ba04
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-05-22 12:19:48 +0300

    checking how hoisting works

---
+++ HoistingCheck.js
@@ -0,0 +1,7 @@
+printTheFunction();
+
+function printTheFunction() {
+  console.log(check);
+  let check = " lkjsdf";
+  console.log("Yes it is hoisted");
+}
---
+++ SetCheck.js
@@ -0,0 +1,7 @@
+const s = new Set();
+s.add({ text: "A" });
+s.add({ text: "A" });
+s.add("S");
+s.add("S");
+
+console.log(s);
