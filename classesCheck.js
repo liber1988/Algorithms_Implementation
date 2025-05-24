6cc2216cccba229f61bedea2ced151ba94902b8b
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-05-24 15:14:15 +0300

    Jvascript execution

--- classesCheck.js
+++ classesCheck.js
@@ -3,29 +3,17 @@ let Person = function (name, gender, birthYear) {
   this.gender = gender;
   this.birthYear = birthYear;
 };
-
 Person.prototype.calcAge = function () {
-  let age = new Date().getFullYear() - this.birthYear;
+  const age = new Date().getFullYear() - this.birthYear;
   console.log(age);
 };
+class Emploeer extends Person {
+  constructor(name, gender, birthYear, empid, salary) {
+    super(name, gender, birthYear);
+    this.empid = empid;
+    this.salary = salary;
+  }
+}
 
-let John = new Person("John", "male", 1990);
-console.log(John);
-
-let Employee = function (name, gender, birthYear, empId, salary) {
-  Person.call(this, name, gender, birthYear);
-  this.empId = empId;
-  this.salary = salary;
-};
-Employee.prototype = Person.prototype;
-Employee.prototype.calcSalary = function () {
-  return this.salary * 12;
-};
-
-Employee.prototype.empDetails = function () {
-  console.log(this.name);
-  console.log(this.empId);
-};
-
-let Mark = new Employee("Mark", "Male", 1995, 101, 12000);
-console.log(Mark);
+let mark = new Emploeer("mark", "male", 1995, 201, 190000);
+console.log(mark);
---
+++ javscriptExecutionCheck.js
@@ -0,0 +1,20 @@
+var name = `steve`;
+var age = 28;
+function first() {
+  var a = "hello";
+  second();
+  var x = a + name;
+}
+
+function second() {
+  var a = "Hello";
+  third();
+  var y = B + name;
+}
+
+function third() {
+  var c = "Hey!";
+  var z = c + name;
+}
+
+first();
